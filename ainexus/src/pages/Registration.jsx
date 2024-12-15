d971c38f5be4fe1ef2ec393ad77d0831b331d9bb
Pranav0786 <pranavsurve.0786@gmail.com>
2024-12-15 20:20:02 +0530

    added proper sequence

--- ainexus/src/pages/Registration.jsx
+++ ainexus/src/pages/Registration.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export const Registration = () => {
   return (
-    <div>Registration</div>
+    // main page
+    <div className='bg-red-300 min-h-screen w-full flex mx-auto items-center justify-center'>
+        {/* actual section */}
+        <div className='bg-gray-700 w-11/12 '>
+            hello
+            {/* left box */}
+            <div>
+
+            </div>
+
+            {/* right box */}
+            <div>
+
+            </div>
+
+        </div>
+    </div>
   )
 }
