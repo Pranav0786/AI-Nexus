c1b4ace1c89e4b95e6b01a91d9bcc65c61d92a4b
Pranav0786 <pranavsurve.0786@gmail.com>
2024-12-23 20:46:24 +0530

    changes

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,7 +1,6 @@
-const {config} = require("dotenv") ;
+require("dotenv").config();
 const mongoose = require("mongoose") ;
 
-require("dotenv").config() ;
 
 const dbConnect = () => {
     mongoose.connect(process.env.DATABASE_URL, {})
