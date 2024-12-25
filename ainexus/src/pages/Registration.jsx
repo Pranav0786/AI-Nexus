c70abce3404b12f747ee99365bc1c7416d8d9e0b
Pranav0786 <pranavsurve.0786@gmail.com>
2024-12-25 16:23:46 +0530

    hosted backend

--- ainexus/src/pages/Registration.jsx
+++ ainexus/src/pages/Registration.jsx
@@ -61,7 +61,7 @@ export const Registration = () => {
     formDataToSend.append('paymentScreenshot', formData.paymentScreenshot);
 
     try {
-      const response = await fetch('http://localhost:5000/api/v1/register', {
+      const response = await fetch('https://ai-nexus-3pk9.onrender.com/api/v1/register', {
         method: 'POST',
         body: formDataToSend,
       });
