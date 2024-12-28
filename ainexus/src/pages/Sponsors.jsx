434487f422a78f4b35625c73d3642646daef675c
Pranav0786 <pranavsurve.0786@gmail.com>
2024-12-28 12:04:39 +0530

    sp

--- ainexus/src/pages/Sponsors.jsx
+++ ainexus/src/pages/Sponsors.jsx
@@ -7,39 +7,33 @@ const sponsors = [
   {
     id: 1,
     logo: img1,
-  },
-  {
-    id: 2,
-    logo: img2,
-  },
-  {
-    id: 3,
-    logo: img3,
-  },
+  }
 ];
 
 const Sponsors = () => {
   return (
-    <section id="sponsors" className="min-h-screen py-12 ">
-      <div className="container mx-auto px-4 text-center">
-        <h1 className="text-4xl md:text-5xl font-extrabold text-gray-800 mb-8">
+    <section id="sponsors" className=" py-12 ">
+      <div className="flex flex-col container mx-auto px-4 text-center mt-10 md:mt-20 mb-20">
+        <h1 className="text-3xl md:text-5xl font-extrabold font-[Roboto] drop-shadow-lg text-red-800 mb-5 uppercase">
           Sponsors
         </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
-          {sponsors.map((sponsor) => (
-            <div
-              key={sponsor.id}
-              className="relative bg-white shadow-lg rounded-xl p-4 hover:scale-105 transition-transform duration-300"
-              style={{ maxWidth: '300px' }}
-            >
-              <img
-                src={sponsor.logo}
-                alt={`Sponsor ${sponsor.id} Logo`}
-                className="w-full h-auto object-cover rounded-md"
-                style={{ boxShadow: '0 4px 10px rgba(0, 0, 0, 0.15)' }}
-              />
-            </div>
-          ))}
+        <div className="flex justify-center items-center   ">
+          <div className="grid grid-cols-1  gap-8 items-center justify-center ">
+            {sponsors.map((sponsor) => (
+              <div
+                key={sponsor.id}
+                className="relative bg-[rgb(105,122,132)] shadow-lg rounded-xl p-3 hover:scale-105 transition-transform duration-300"
+
+              >
+                <img
+                  src={sponsor.logo}
+                  alt={`Sponsor ${sponsor.id} Logo`}
+                  className="w-full h-auto md:h-96 object-cover rounded-md"
+                  style={{ boxShadow: '0 4px 10px rgba(0, 0, 0, 0.15)' }}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
