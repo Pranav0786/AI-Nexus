63156058e2698b00f2dd43879c5a5f7c9c0ef94f
Pranav0786 <pranavsurve.0786@gmail.com>
2025-01-11 19:30:40 +0530

    modify backend

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const {Team} = require("../models/User")
 const { register, uploadMiddleware , getUserDetails  } = require("../controllers/Auth");
 
 const router = express.Router();
 
 router.post("/register", uploadMiddleware, register);
 router.get("/teams/:teamId", getUserDetails);
+router.get("/teams", async (req, res) => {
+    try {
+      const teams = await Team.find().exec(); 
+      if (teams.length === 0) {
+        return res.status(404).json({ message: 'No teams found' });
+      }
+      res.status(200).json(teams); 
+    } catch (error) {
+      res.status(500).json({ message: 'Server error', error });
+    }
+  });
 
 module.exports = router;
